79103259564a8c0eac8ab8be3ed419a8ddddaab6
João VIctor Yamurri <148209487+Nimdokz@users.noreply.github.com>
2024-01-13 17:38:35 -0300

    Delete Profile.py

--- Profile.py
+++
@@ -1,7 +0,0 @@
-age = (int("18"))
-first_name = (str("Joe"))
-last_name = (str("Mom"))
-gender = (str("Male"))
-height = (float("1.98"))
-nationality= (str("Brazilian"))
-print("age: ", str(age), "\nname: ", first_name, last_name, "\nGender: ", gender, "\nheight :", height,  "\nnationality: ", nationality)
